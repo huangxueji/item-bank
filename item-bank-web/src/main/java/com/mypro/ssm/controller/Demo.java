f664d54cd7161cac1b1cfd7bafd3b55c86774a03
黄雪记 <1075950740@qq.com>
2019-06-24 00:32:01 +0800

    添加了一个打印

--- item-bank-web/src/main/java/com/mypro/ssm/controller/Demo.java
+++ item-bank-web/src/main/java/com/mypro/ssm/controller/Demo.java
@@ -3,5 +3,6 @@ package com.mypro.ssm.controller;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("ahahahahaha");
+        System.out.println("yyyyyyyy");
     }
 }
